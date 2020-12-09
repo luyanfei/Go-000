4b853395af24a8e70defc6a4eb55df5d25030e7c
luyanfei <luyanfei78@163.com>
2020-12-09 23:41:09 +0800

    main.go revised.

--- Week03/main.go
+++ Week03/main.go
@@ -6,41 +6,38 @@ import (
 	"golang.org/x/sync/errgroup"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
-func listen1(done <-chan interface{}) error {
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println("listen1 is working...")
-		select {
-		case <-done:
-			return errors.New("listen1 canceled")
-		default:
-		}
-	}
-}
-
-func listen2(done <-chan interface{}) error {
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println("listen2 is working...")
-		select {
-		case <-done:
-			return errors.New("listen2 canceled")
-		default:
-		}
-	}
-}
-
 func main() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt)
+	signal.Notify(c, syscall.SIGINT)
 
 	g := new(errgroup.Group)
 	done := make(chan struct{})
-	g.Go(listen1)
-	g.Go(listen2)
+	g.Go(func() error {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("listen1 is working...")
+			select {
+			case <-done:
+				return errors.New("listen1 canceled")
+			default:
+			}
+		}
+	})
+	g.Go(func() error {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("listen2 is working...")
+			select {
+			case <-done:
+				return errors.New("listen2 canceled")
+			default:
+			}
+		}
+	})
 	if _, ok := <-c; ok {
 		close(done)
 	}
